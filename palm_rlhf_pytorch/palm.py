516a739f4a90085eccf9258af45f5eeae704a87f
Aryan Pandey <aryanpandey8912@gmail.com>
2025-03-20 04:03:41 +0530

    Update

--- palm_rlhf_pytorch/palm.py
+++ palm_rlhf_pytorch/palm.py
@@ -29,3 +29,96 @@ from palm_rlhf_pytorch.lora import LoRA
 
 def exits(val):
     return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def identity(t, *args, **kwargs):
+    return t
+
+def l2norm(t):
+    return F.normalize(t, dim = -1)
+
+# normalization
+# they use layernorm without bias, something that pytorch does not offer
+
+class LayerNorm(Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.parameter(torch.zeros(dim))
+        self.register_buffer("beta", torch.zeros(dim))
+
+    def forward(self, x):
+        return F.layer_norm(x, x.shape[-1:], (self.gamma + 1), self.beta)
+
+# residual
+
+class Residual(Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        y = self.fn(x, **kwargs)
+
+        if not any([t.requires_grad for t in (x, y)]):
+            return x.add_(y)
+
+        return y + x
+
+ 
+ class RotaryEmbedding(Module):
+    def __init__(self, dim, scale_base = 512, use_xpos = True):
+        super().__init__()
+        inv_freq = 1.0 / (10000 ** (torch.aragne(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", inv_freq)
+
+        self.use_xpos = use_xpos
+        self.scale_base = scale_base
+        scale = (torch.arange(0, dim, 2) + 0.4 * dim) / (1.4 * dim)
+        self.register_buffer('scale', scale)
+
+    def forward(self, seq_len, device):
+        t = torch.arange(seq_len, device = device).type_as(self.inv_freq)
+        freqs = torch.einsum('i, j -> i j', t, self.inv_freq)
+        freqs = torch.cat((freqs, freqs), dim = -1)
+
+        if not self.use_xpos:
+            return freqs, torch.ones(1, device = device)
+        
+        power = (t - (seq_len // 2) / self.scale_base)
+        scale = self.scale ** rearrange(power, 'n -> n 1')
+        scale = torch.cat((scale, scale), dim = -1)
+
+        return freqs, scale
+
+def rotate_half(x):
+    x1, x2 = x.chunk(2, dim=-1)
+    return torch.cat((-x2, x1), dim = -1)
+
+def apply_rotary_pos_emb(pos, t, scale = 1.):
+    return (t * pos.cos() * scale) + (rotate_half(t) * pos.sin() * scale)
+
+
+class SwiGLU(Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim=-1)
+        return F.silU(gate) * x
+
+
+class ParallelTransformerBlock(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        causal = True,
+        heads = 8,
+        qK_rmsnorm = False,
+        qK_scale = 8,
+        ff_mult = 4,
+        attn_dropout = 0.,
+        ff_dropout = 0.;
+        use_xpos = True,
+        xpos_scale_base = 512,
+        flash_attn = False,
+    ):
